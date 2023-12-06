b700e3f33420e3adb377f26ed9ecff62651922fb
NickWang <brotherstyx@gmail.com>
2023-12-06 18:59:00 +0800

    feat: 🎸 GetContactExternalUserId

--- commonjs/generated/puppet.cjs.d.ts
+++ commonjs/generated/puppet.cjs.d.ts
@@ -43,6 +43,7 @@ export * from '../../out/wechaty/puppet/util_pb.js'
 export * from '../../out/wechaty/puppet/verify-code_pb.js'
 export * from '../../out/wechaty/puppet/call_pb.js'
 export * from '../../out/wechaty/puppet/chat-history_pb.js'
+export * from '../../out/wechaty/puppet/wecom_pb.js'
 
 export * from '../../out/wechaty/puppet_grpc_pb.js'
 export * from '../../out/wechaty/puppet_pb.js'
